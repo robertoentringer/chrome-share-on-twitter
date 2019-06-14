6fd74c58e82ff1d6e706d101150e3a0c8aa06090
Roberto Entringer <r.entringer@gmail.com>
2019-06-15 01:48:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Share on Twitter
+
+A simple way to share current tab, links or the selected text on Twitter.  Using toolbar button and context menu.
+
+Add to Chrome : 
+[chrome web store](https://chrome.google.com/webstore/detail/share-on-twitter/ndhigigiemdlgngacocmigbjenblipdo)
+
+Video demo :
+https://www.youtube.com/watch?v=WRYhQH_IpbE
+
+Screenshots : 
+
+[![screenshot.jpg](screenshot.jpg?raw=true)](https://chrome.google.com/webstore/detail/share-on-twitter/ndhigigiemdlgngacocmigbjenblipdo)
+
+[![screenshot2.jpg](screenshot2.jpg?raw=true)](https://chrome.google.com/webstore/detail/share-on-twitter/ndhigigiemdlgngacocmigbjenblipdo)
---
+++ src/background.js
@@ -0,0 +1,51 @@
+const tweet = async (url, title = "") => {
+  const resource = new URL("https://twitter.com")
+  const compose = new URL("compose/tweet", resource)
+  const login = new URL("login", resource)
+  const intent = new URL("intent/tweet", resource)
+
+  compose.searchParams.append("text", title)
+  compose.searchParams.append("url", url)
+
+  login.searchParams.append("redirect_after_login", compose.href)
+
+  intent.searchParams.append("text", title)
+  intent.searchParams.append("url", url)
+
+  let endpoint = intent.href
+
+  await fetch(compose).then(resp => {
+    if (resp.ok) endpoint = compose.href
+    else if (resp.status == 404) {
+      chrome.cookies.set({ name: "rweb_optin", url: "https://twitter.com", value: "on" })
+      endpoint = login.href
+    }
+  })
+
+  chrome.tabs.create({ url: endpoint })
+}
+
+const browserAction = tab => {
+  chrome.tabs.executeScript({ code: "window.getSelection().toString();" }, ([selection]) =>
+    tweet(tab.url, selection || tab.title)
+  )
+}
+
+const onInstalled = () => {
+  const contexts = ["page", "selection", "link", "image"]
+  for (const context of contexts) {
+    const title = `Share ${context} on Twitter`
+    chrome.contextMenus.create({ title, contexts: [context], id: context })
+  }
+}
+
+const contextMenus = (data, tab) => {
+  if ("page" == data.menuItemId) tweet(data.pageUrl, tab.title)
+  if ("link" == data.menuItemId) tweet(data.linkUrl)
+  if ("selection" == data.menuItemId) tweet(tab.url, data.selectionText)
+  if ("image" == data.menuItemId) tweet(data.srcUrl)
+}
+
+chrome.browserAction.onClicked.addListener(browserAction)
+chrome.contextMenus.onClicked.addListener(contextMenus)
+chrome.runtime.onInstalled.addListener(onInstalled)
