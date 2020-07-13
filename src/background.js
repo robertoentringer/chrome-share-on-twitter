3f1fae3fa2734286dfcc8bd404ac2999f915296b
Roberto Enringer <r.entringer@gmail.com>
2020-07-13 15:47:50 +0200

    bump npm packages, install zipack and run lint

--- src/background.js
+++ src/background.js
@@ -1,23 +1,23 @@
-const tweet = async (url, title = "") => {
-  const resource = new URL("https://twitter.com")
-  const compose = new URL("compose/tweet", resource)
-  const login = new URL("login", resource)
-  const intent = new URL("intent/tweet", resource)
+const tweet = async (url, title = '') => {
+  const resource = new URL('https://twitter.com')
+  const compose = new URL('compose/tweet', resource)
+  const login = new URL('login', resource)
+  const intent = new URL('intent/tweet', resource)
 
-  compose.searchParams.append("text", title)
-  compose.searchParams.append("url", url)
+  compose.searchParams.append('text', title)
+  compose.searchParams.append('url', url)
 
-  login.searchParams.append("redirect_after_login", compose.href)
+  login.searchParams.append('redirect_after_login', compose.href)
 
-  intent.searchParams.append("text", title)
-  intent.searchParams.append("url", url)
+  intent.searchParams.append('text', title)
+  intent.searchParams.append('url', url)
 
   let endpoint = intent.href
 
-  await fetch(compose).then(resp => {
+  await fetch(compose).then((resp) => {
     if (resp.ok) endpoint = compose.href
     else if (resp.status == 404) {
-      chrome.cookies.set({ name: "rweb_optin", url: "https://twitter.com", value: "on" })
+      chrome.cookies.set({ name: 'rweb_optin', url: 'https://twitter.com', value: 'on' })
       endpoint = login.href
     }
   })
@@ -25,14 +25,14 @@ const tweet = async (url, title = "") => {
   chrome.tabs.create({ url: endpoint })
 }
 
-const browserAction = tab => {
-  chrome.tabs.executeScript({ code: "window.getSelection().toString();" }, ([selection]) =>
+const browserAction = (tab) => {
+  chrome.tabs.executeScript({ code: 'window.getSelection().toString();' }, ([selection]) =>
     tweet(tab.url, selection || tab.title)
   )
 }
 
 const onInstalled = () => {
-  const contexts = ["page", "selection", "link", "image"]
+  const contexts = ['page', 'selection', 'link', 'image']
   for (const context of contexts) {
     const title = `Share ${context} on Twitter`
     chrome.contextMenus.create({ title, contexts: [context], id: context })
@@ -40,10 +40,10 @@ const onInstalled = () => {
 }
 
 const contextMenus = (data, tab) => {
-  if ("page" == data.menuItemId) tweet(data.pageUrl, tab.title)
-  if ("link" == data.menuItemId) tweet(data.linkUrl)
-  if ("selection" == data.menuItemId) tweet(tab.url, data.selectionText)
-  if ("image" == data.menuItemId) tweet(data.srcUrl)
+  if ('page' == data.menuItemId) tweet(data.pageUrl, tab.title)
+  if ('link' == data.menuItemId) tweet(data.linkUrl)
+  if ('selection' == data.menuItemId) tweet(tab.url, data.selectionText)
+  if ('image' == data.menuItemId) tweet(data.srcUrl)
 }
 
 chrome.browserAction.onClicked.addListener(browserAction)
